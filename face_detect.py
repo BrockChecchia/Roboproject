3a6c913b614caafbe6ce7c815c0016aa25fa9411
Tyra Wilson <tyrawilson134@gmail.com>
2020-11-06 12:46:05 -0500

    Update face_detect.py
    change folder name to pic

--- face_detect.py
+++ face_detect.py
@@ -3,7 +3,7 @@
 import cv2
 #faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 faceCascade = cv2.CascadeClassifier(r"C:/Users/tyraw/AppData/Local/Programs/Python/Python38-32/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
-img = cv2.imread("rscrs/lena.png")
+img = cv2.imread("rsrcs/lena.png")
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
